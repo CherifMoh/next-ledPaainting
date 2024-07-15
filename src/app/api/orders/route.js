5e0c6639f7d287f42e7a03c826457000eeada63f
CherifMoh <mircho.dev@gmail.com>
2024-07-15 13:59:12 +0100

    d

--- src/app/api/orders/route.js
+++ src/app/api/orders/route.js
@@ -8,7 +8,16 @@ export async function GET() {
   try {
 
     await dbConnect()
-    const result = await Order.find().sort({ _id: -1 })
+    const startDate = new Date("2024-07-14T00:00:00.000Z");
+    const endDate = new Date("2024-07-15T00:00:00.000Z");
+
+    const result = await Order.find({
+      createdAt: {
+        $gte: startDate,
+        $lt: endDate
+      }
+    }).sort({ _id: -1 });
+
     return Response.json(result)
 
   } catch (err) {
