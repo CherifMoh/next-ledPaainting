408f1e92825d42485da5a8a4eb07da7824cd6654
CherifMoh <mircho.dev@gmail.com>
2024-08-04 13:37:02 +0100

    fix

--- src/app/admin/orders/page.jsx
+++ src/app/admin/orders/page.jsx
@@ -452,7 +452,7 @@ function Orders() {
 
     async function addToTsl(order) {
 
-        if(!order.reference||(!order.totalPrice && !order.shippingPrice) ||!order.name||!order.phoneNumber||!order.adresse||!order.commune){
+        if(!order.reference ||!order.name||!order.phoneNumber||!order.adresse||!order.commune){
             return setErrorNotifiction("couldn't set the order in TSL")
         }
         
@@ -495,7 +495,7 @@ function Orders() {
             code_wilaya: wilayaCode,
             fragile:1,
             remarque:order.deliveryNote,
-            montant:order.totalPrice === 0 ? order.shippingPrice : order.totalPrice,
+            montant:order.totalPrice,
             stop_desk:order.shippingMethod === 'مكتب' ? 1 : 0,
             type:1,
         }
