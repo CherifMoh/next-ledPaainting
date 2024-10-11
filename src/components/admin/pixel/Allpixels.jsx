d08dbedebf3dd3c29868a87860daca3e1756785b
CherifMoh <mircho.dev@gmail.com>
2024-10-11 10:42:11 +0100

    drawlys

--- src/components/admin/pixel/Allpixels.jsx
+++ src/components/admin/pixel/Allpixels.jsx
@@ -7,7 +7,7 @@ async function fetchPixels() {
     const agent = new https.Agent({  
         rejectUnauthorized: false
     });
-    const res = await axios.get('https://localhost:3000/api/pixel', {
+    const res = await axios.get('https://drawlys.com/api/pixel', {
         httpsAgent: agent
     });
     return res.data;
